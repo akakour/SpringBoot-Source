5457bb73f5d0c4f13b07d5ad68a53e43cf135dbb
Akakour <akakour@qq.com>
2020-02-20 22:06:47 +0900

    add all

---
+++ spring-boot-project/spring-boot-cli/target/homebrew/springboot.rb
@@ -0,0 +1,27 @@
+require 'formula'
+
+class Springboot < Formula
+  homepage 'https://spring.io/projects/spring-boot'
+  url 'https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.2.2.RELEASE/spring-boot-cli-2.2.2.RELEASE-bin.tar.gz'
+  version '2.2.2.RELEASE'
+  sha256 'f096d2a4ece50326613efad7bd4b7a02e4230bff3b35b766d8ab3debe50881d1'
+  head 'https://github.com/spring-projects/spring-boot.git'
+
+  if build.head?
+    depends_on 'maven' => :build
+  end
+
+  def install
+    if build.head?
+      Dir.chdir('spring-boot-cli') { system 'mvn -U -DskipTests=true package' }
+      root = 'spring-boot-cli/target/spring-boot-cli-*-bin/spring-*'
+    else
+      root = '.'
+    end
+
+    bin.install Dir["#{root}/bin/spring"]
+    lib.install Dir["#{root}/lib/spring-boot-cli-*.jar"]
+    bash_completion.install Dir["#{root}/shell-completion/bash/spring"]
+    zsh_completion.install Dir["#{root}/shell-completion/zsh/_spring"]
+  end
+end
